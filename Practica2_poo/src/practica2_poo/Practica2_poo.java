87a868ca4ee073cee717aa050f2e006026fac1a8
Alumno21 <Alumno21@PC21-063048>
2024-11-07 16:45:46 -0600

    prueba

---
+++ Practica2_poo/src/practica2_poo/Practica2_poo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2_poo;
+
+/**
+ *
+ * @author Alumno21
+ */
+public class Practica2_poo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+    
+}
